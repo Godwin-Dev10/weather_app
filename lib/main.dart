5c8378334db1d0449cdf56b6cf68feab6fd06b45
Godwin .F. Sampson <sampsongodwin10@gmail.com>
2024-06-23 22:55:11 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,25 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_app/screens/home_screen.dart';
 
 void main() {
-  runApp(const ProviderScope(
+  runApp(
+    const ProviderScope(
       child: MyApp(),
-  ),);
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-            title: 'Weather App',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF1D85E4)),
-            ),
-            home: HomeScreen(),
-          );
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF1D85E4)),
+      ),
+      home: HomeScreen(),
+    );
   }
 }
-
